5df67f97046ac92a23eb5197d34a532d5340742b
hossein <143866200+hos3sein@users.noreply.github.com>
2023-10-23 22:13:53 +0330

    Add files via upload

---
+++ network scanner with scapy.py
@@ -0,0 +1,38 @@
+from scapy.all import *
+
+ip = IP()
+
+# ip.show()
+
+
+
+# destination ip address
+target_ip = '192.168.1.1/24'
+
+# create arp packet
+arp = ARP(pdst = target_ip)
+
+# create the ether broadcast packet
+# ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
+
+ether = Ether(dst = "ff:ff:ff:ff:ff:ff")
+
+# stack them
+packet = ether/arp
+
+# now we send the packets in layer 2
+
+res = srp(packet , timeout = 3)[0]
+
+
+clients = []
+
+for sent , recieved in res:
+	clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+
+# now we just need to print the resault to see the devicess
+
+print('available devices')
+for client in clients:
+	print("{:16}    {}".format(client['ip'], client['mac']))
